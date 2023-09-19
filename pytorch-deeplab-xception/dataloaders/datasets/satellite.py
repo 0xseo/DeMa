7f2271ba246bb07951f9c283afc025b88ec58363
0xseo <87014012+0xseo@users.noreply.github.com>
2023-09-19 17:19:29 +0900

    Add files via upload

---
+++ pytorch-deeplab-xception/dataloaders/datasets/satellite.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import cv2
+import torch.utils.data as data
+
+# RLE 디코딩 함수
+def rle_decode(mask_rle, shape):
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape)
+
+# RLE 인코딩 함수
+def rle_encode(mask):
+    pixels = mask.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+class SatelliteDataset(data.Dataset):
+    def __init__(self, csv_file, transform=None, infer=False):
+        self.data = pd.read_csv(csv_file)
+        self.transform = transform
+        self.infer = infer
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path = self.data.iloc[idx, 1]
+        image = cv2.imread('.'+img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.infer:
+            if self.transform:
+                image = self.transform(image=image)['image']
+            return image
+
+        mask_rle = self.data.iloc[idx, 2]
+        mask = rle_decode(mask_rle, (image.shape[0], image.shape[1]))
+
+        if self.transform:
+            augmented = self.transform(image=image, mask=mask)
+            image = augmented['image']
+            mask = augmented['mask']
+
+        return image, mask
